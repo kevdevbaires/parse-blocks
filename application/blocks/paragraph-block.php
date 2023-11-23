e81ee152859c53684ddbd570426bd2265d992eff
kevdevbaires <kchavez@redventures.com>
2023-11-23 14:13:44 -0600

    update indent to spaces

--- application/blocks/paragraph-block.php
+++ application/blocks/paragraph-block.php
@@ -4,23 +4,23 @@ namespace CNET\Bridge\Blocks;
 
 class Paragraph_Block extends Block_Parser implements Block_Interface {
 
-	public function __construct() {
+    public function __construct() {
         add_action('init',function () {
             $this->init();
         });
-	}
+    }
 
     public function init() {
         add_filter('bridge_block_filter', [$this, 'block_parser']);
     }
 
-	public function block_parser($content) {
+    public function block_parser($content) {
         $this->blocks = $content;
 
         $filteredBlocks = $this->filter_by_block_name('core/paragraph');
 
         return $this->to_html($filteredBlocks);
-	}
+    }
 
     public function to_html($filteredBlocks) {
         foreach ($filteredBlocks as $key => $block) {
