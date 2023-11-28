7c0424257c3adac1b713bc7a312fc77d555fe2b8
kevdevbaires <kchavez@redventures.com>
2023-11-28 15:25:06 -0600

    added class to handle list blocks, added class to handle heading blocks

---
+++ application/blocks/list-block.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace CNET\Bridge\Blocks;
+
+class List_Block extends Block_Parser implements Block_Interface {
+
+	public function __construct() {
+		add_action('init',function () {
+			$this->init();
+		});
+	}
+
+	function init() {
+		add_filter('bridge_block_filter', [$this, 'block_parser']);
+	}
+
+	function block_parser($content) {
+		$this->blocks = $content;
+
+		$filteredBlocks = $this->filter_by_block_name('core/list');
+
+		return $this->to_html($filteredBlocks);
+	}
+
+	function to_html($filteredBlocks) {
+		foreach ($filteredBlocks as $key => $block) {
+			$this->blocks[$key] = [
+				'blockName' => 'parse',
+				'block_html' => $this->get_attributes($block)
+			];
+		}
+
+		return $this->blocks;
+	}
+
+	function get_attributes($block) {
+		$inner_html = $this->parse_inner_html($block);
+
+		if (!$inner_html) return null;
+
+		return $this->loop_inner_blocks($block);
+	}
+
+	function loop_inner_blocks($block){
+		if (!is_array($block) || count($block) == 0) return '';
+
+		$list_items = [];
+
+		$opening_tag = reset($block['innerContent']);
+		$closing_tag = end($block['innerContent']);
+
+		if (is_array($block['innerBlocks']) || count($block['innerBlocks']) > 0){
+			foreach($block['innerBlocks'] as $key => $list_item_block) {
+
+				if(count($list_item_block['innerBlocks']) > 0) {
+					$this->loop_inner_blocks($list_item_block);
+				}
+
+				$list_items[$key] = trim(preg_replace('/\s+/', ' ', $list_item_block['innerHTML']));
+			}
+		}
+
+		return $opening_tag . implode($list_items) . $closing_tag;
+	}
+}
